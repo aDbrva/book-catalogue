e0fa29010aa6b03248ae659dcffb042e1495aff6
Anton Dibrova <anton.dibrova1113@gmail.com>
2023-07-31 20:12:23 +0300

    added some routes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
 
 const PORT = 3000
 
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
 
 app.get("/", (req, res) => {
-    res.send('Hello world!')
+    // console.log(createPath('index'))
+    res.sendFile(createPath('index'))
+})
+
+app.get("/add-book", (req, res) => {
+    res.sendFile(createPath('add-book'))
+})
+
+app.use((req, res) => {
+    res.status(404)
+        .sendFile(createPath('error'))
 })
 
 app.listen(PORT, () => {
